be70cb9b18551d23dfd9dfde25e33e3dec34968c
Prakhar Katiyar <56363476+prakharkatiyar181@users.noreply.github.com>
2023-07-25 21:03:40 +0530

    Time: 1 ms (99.82%), Space: 48.6 MB (52.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,28 +1,27 @@
-// import java.util.collections;
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int value = 0;
-        int key = 0;
-        int maxValue = 0;
         int el = 0;
-        HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
-        for( int i=0; i<n ;i++) {
-            value = mpp.getOrDefault(nums[i],0);
-            value += 1;
-            mpp.put(nums[i], value);
-        }
-        for(Map.Entry<Integer, Integer> val: mpp.entrySet()) {
-            if(maxValue < val.getValue()) {
-                maxValue = val.getValue();
-                el = val.getKey();
+        int cnt = 0;
+        int n = nums.length;
+        int i = 0;
+        int cnt2 = 0;
+        for( i = 0; i < n ; i++) {
+            if(cnt == 0) {
+                el = nums[i];
+                cnt = 1;
+            } else if( el == nums[i]) {
+                cnt++;
+            } else {
+                cnt--;
             }
         }
-        if( maxValue > n/2) {
-           return el; 
+        for( i = 0; i < n; i++) {
+            if(nums[i] == el)
+                cnt2++;
+        }
+        if( cnt2 > n/2 ) {
+            return el;
         }
         return -1;
-        // int max = collections.max(mpp.keySet())
-        
     }
 }
