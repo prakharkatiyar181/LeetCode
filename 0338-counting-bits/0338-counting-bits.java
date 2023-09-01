2289a098ef243d50de735d33a8cf4aed663351d1
Prakhar Katiyar <56363476+prakharkatiyar181@users.noreply.github.com>
2023-09-01 09:49:09 +0530

    Time: 3 ms (35.70%), Space: 46.2 MB (88.58%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public int[] countBits(int n)
+    {
+        int[] list1 = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            int sum = CountOnesInBinary(i);
+            list1[i] = sum;
+        }
+        return list1;
+    }
+
+    private int CountOnesInBinary(int num)
+    {
+        int count = 0;
+        while (num > 0)
+        {
+            count += num & 1;
+            num >>= 1;
+        }
+        return count;
+    }
+}
